7ee33155d8c765012237a16013f7316438743d99
Piyanut Rotcharoensap <110012203+Piyanut012@users.noreply.github.com>
2024-09-30 20:42:16 +0700

    Add templates 1

--- fitness_management/fitmark/views.py
+++ fitness_management/fitmark/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'signup.html', context)
+
+
+
